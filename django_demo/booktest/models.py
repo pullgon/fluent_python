ffdbf396606a14dd7052dd311bf2acde15830597
gq <91thenx@gmail.com>
2021-03-07 02:58:06 +0800

    git test

---
+++ django_demo/booktest/models.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8-*-
+# Author: gq
+# CreatDate: 2021/3/7 2:56
+# Description: Create your models here.
+
+from django.db import models
+
+if __name__ == '__main__':
+    pass
---
+++ django_demo/booktest/my_test.py
@@ -0,0 +1,7 @@
+# -*-coding:utf-8-*-
+# Author: gq
+# CreatDate: 2021/3/7 2:56
+# Description:
+
+if __name__ == '__main__':
+    pass
