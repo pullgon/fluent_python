da7d21e803443a6a7de3090fb12591038928b619
gq <91thenx@gmail.com>
2021-03-17 01:25:12 +0800

    pre-commit

--- django_demo/booktest/admin.py
+++ django_demo/booktest/admin.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+# Register your models here.
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import BookInfo, HeroInfo
+
+
+# 自定义模型类
+class BookInfoAdmin(admin.ModelAdmin):
+    """图书模型自定义管理类"""
+
+    list_display = ["id", "title", "pub_date"]
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    """图书模型自定义管理类"""
+
+    list_display = ["id", "name", "age", "gender", "comment", "hero_book"]
+
+
+# 注册模型类
+admin.site.register(BookInfo, BookInfoAdmin)
+admin.site.register(HeroInfo, HeroInfoAdmin)
--- django_demo/booktest/models.py
+++ django_demo/booktest/models.py
@@ -8,7 +8,25 @@ from django.db import models
 
 class BookInfo(models.Model):
     """图书模型类"""
-    # 图书名称
-    title = models.CharField(max_length=20)  # 字符串，最大长度为20
-    # 出版日期
-    pub_date = models.DateField()
+
+    title = models.CharField(max_length=20, help_text="图书名称")  # 字符串，最大长度为20
+    pub_date = models.DateField(help_text="出版日期")
+
+    def __str__(self):
+        return self.title
+
+
+class HeroInfo(models.Model):
+    """英雄人物类"""
+
+    name = models.CharField(max_length=50, help_text="英雄名")
+    age = models.IntegerField(help_text="英雄性别")
+    # 0 为女， 1 为 男
+    gender = models.IntegerField(help_text="英雄性别")
+    comment = models.CharField(max_length=255, help_text="备注")
+    hero_book = models.ForeignKey(
+        "BookInfo", on_delete=models.CASCADE, related_name="book"
+    )
+
+    def __str__(self):
+        return self.name
